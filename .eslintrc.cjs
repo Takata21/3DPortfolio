5cd6a3c2430e66d71b026cdb814d93f140051122
Manuel Takata <takata211011@gmail.com>
2023-03-18 21:33:31 -0500

    project setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+const Rules = {
+  OFF: 'off',
+  WARN: 'warn',
+  ERROR: 'ERROR',
+}
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'standard', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/prop-types': Rules.OFF,
+  },
+}
